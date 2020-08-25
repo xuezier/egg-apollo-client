ecdc664ca602cd5c25a64e34778c96c1f024fb57
yuli <yuli@g7.com.cn>
2020-08-25 17:43:59 +0800

    fix: config.apollo优先加载js文件

--- lib/loadTs.ts
+++ lib/loadTs.ts
@@ -3,9 +3,9 @@ import * as fs from 'fs';
 declare function require(name:string);
 
 export default function loadTs(path: string) {
-    if(fs.existsSync(path) && process.env.NODE_ENV === 'development') {
+    if(fs.existsSync(path)) {
         return require(path);
     } else {
-        return require(path.replace(/\.ts$/, '.js'));
+        return require(path.replace(/\.js$/, '.ts'));
     }
 }
